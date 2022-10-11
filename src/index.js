95acd8e3e1e14048e2fabe6cd832c2f2aead2a79
tsering651 <tseringwang4039@gmail.com>
2022-10-11 16:10:13 +0530

    Added Home Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
+import Home from './common/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
+  <Home/>
   </React.StrictMode>
 );
 
