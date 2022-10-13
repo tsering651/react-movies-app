a9026083a14a8d8665d6a762d0c6fc7359e10fdc
tsering651 <tseringwang4039@gmail.com>
2022-10-13 19:50:17 +0530

    Added logo

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,14 +1,21 @@
 import React,{Component} from "react";
 import Button from '@mui/material/Button';
+import './Header.css'
+import logo from '../../assets/logo.svg';
+
 
 class Header extends Component{
     render(){
         return(
-            <div>
-               <Button variant="contained"  >Login</Button>
-        
-      
+            <header className="app-header">
+           <img src={logo} className="app-logo" alt="Movies App Logo" />
+            <div className="login-button">
+           
+                <Button variant="contained" >
+                    Login
+                </Button>
             </div>
+        </header>
         )
     }
 }
