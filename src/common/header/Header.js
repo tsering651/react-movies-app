d05fa6b42b130804182d7069bdf48ca24143a428
tsering651 <tseringwang4039@gmail.com>
2022-10-13 19:17:21 +0530

    added login button in header

---
+++ src/common/header/Header.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+import Button from '@mui/material/Button';
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+               <Button variant="contained"  >Login</Button>
+        
+      
+            </div>
+        )
+    }
+}
+export default Header;
